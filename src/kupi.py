d2cbf87fa129ff7a52ab4e79dd5922dffa4bd71e
Я <veronika.havlova@mensagymnazium.cz>
2024-12-22 00:31:53 +0100

    addthefuckingdatabasetogitignore

--- src/kupi.py
+++ src/kupi.py
@@ -1,8 +1,8 @@
 '''
 TODO: WRITE OWN PARSER!!!
 The pykupi parser im using does not provide access to drink amount
-eg. 6 bottles -> I am filtering those offers out;
-This file will eventually get a big rewrite
+eg. 6 bottles = higher price -> I am filtering those offers out;
+This file will (eventually) get a big rewrite
 '''
 
 import asyncio
@@ -13,20 +13,28 @@ from models import session, Drink, Store, store_drink_association
 async def update():
     parser = KupiParser()
     drinks = session.query(Drink).all()
-    stores = {store.name for store in session.query(Store).all()}
+    stores = session.query(Store).all()
+    stores_list = {store.name for store in session.query(Store).all()}
 
-    for drink in drinks:
-        raw = await parser.get_prices(drink.Name)
+    try:
+        for drink in drinks:
+            raw = await parser.get_prices(drink.name)
 
-        # filter out offers from obscure stores nobody knows
-        filtered_offers = [offer for offer in raw.offers if offer.offered_by.lower() in stores]
-        
-        if filtered_offers:
-            cheapest = filtered_offers[0]
-            if cheapest < drink.normal_cost:
-                drink.discount = True
-                drink.discount_cost = cheapest
+            # filter out offers from obscure stores nobody knows
+            filtered_offers = [offer for offer in raw.offers if offer.offered_by.lower() in stores_list]
 
-    await parser.session.close()
+            if filtered_offers:
+                cheapest = filtered_offers[0].price
+                if cheapest < drink.normal_cost:
+                    drink.discount = True
+                    drink.discount_cost = cheapest
 
-asyncio.run(update())
+                print(drink.discount_cost) #test
+
+            session.commit()
+
+    finally:
+        await parser.session.close()
+
+if __name__ == "__main__":
+    asyncio.run(update())
